a9d975186f15ad787f2c5747bc04d0904decd51b
Naman Rajput <namanrajput801@gmail.com>
2024-04-20 14:45:23 +0530

    feat:changes in delhi weekend schema

--- models/weekend/domestic/delhiWeekendData.js
+++ models/weekend/domestic/delhiWeekendData.js
@@ -1,3 +1,38 @@
+// const mongoose = require("mongoose");
+
+// const reviewSchema = new mongoose.Schema({
+//   review: String,
+//   name: String,
+//   date: String,
+// });
+
+// const itinerarySchema = new mongoose.Schema({
+//   day: String,
+//   heading: String,
+//   bulletPoints: [String],
+// });
+
+// const DelhiWeekendSchema = new mongoose.Schema({
+//   _id: mongoose.Schema.Types.ObjectId,
+//   heading: String,
+//   subHeading: String,
+//   duration: String,
+//   maxPeople: Number,
+//   minAge: String,
+//   pickup: String,
+//   overview: String,
+//   exclusions: [String],
+//   inclusions: [String],
+//   reviews: [reviewSchema],
+//   mapSrc: String,
+//   reviewHeading: String,
+//   previousPrice: String,
+//   price: String,
+//   itinerary: [itinerarySchema],
+// });
+
+// module.exports = mongoose.model("DelhiWeekend", DelhiWeekendSchema);
+
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
@@ -9,7 +44,6 @@ const reviewSchema = new mongoose.Schema({
 const itinerarySchema = new mongoose.Schema({
   day: String,
   heading: String,
-  bulletPoints: [String],
 });
 
 const DelhiWeekendSchema = new mongoose.Schema({
@@ -17,13 +51,10 @@ const DelhiWeekendSchema = new mongoose.Schema({
   heading: String,
   subHeading: String,
   duration: String,
-  maxPeople: Number,
-  minAge: String,
-  pickup: String,
-  overview: String,
   exclusions: [String],
   inclusions: [String],
   reviews: [reviewSchema],
+  visaPolicy: [String],
   mapSrc: String,
   reviewHeading: String,
   previousPrice: String,
@@ -31,4 +62,4 @@ const DelhiWeekendSchema = new mongoose.Schema({
   itinerary: [itinerarySchema],
 });
 
-module.exports = mongoose.model("DelhiWeekend", DelhiWeekendSchema);
+module.exports = mongoose.model("delhiWeekend", DelhiWeekendSchema);
